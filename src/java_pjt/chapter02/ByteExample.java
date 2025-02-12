26101bc1e73067ac20e9c5b6fffd33ceb4564fe8
manamjong <nskynet5374@naver.com>
2025-02-12 15:05:20 +0900

    byte크기 표현

---
+++ src/java_pjt/chapter02/ByteExample.java
@@ -0,0 +1,13 @@
+package java_pjt.chapter02;
+
+public class ByteExample {
+    public static void main(String[] args) {
+//        byte var1 = -128;
+        
+        // byte의 범위를 벗어나서 오류 발생
+        // byte var1 = -129;
+        byte var1 = -30;
+
+        System.out.println(var1);
+    }
+}
