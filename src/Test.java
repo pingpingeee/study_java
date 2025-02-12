5780c526618bab93bc9312c5564f6337ac5683b0
manamjong <nskynet5374@naver.com>
2025-02-12 15:08:27 +0900

    문제푸는중

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Test {
@@ -8,16 +9,26 @@ public class Test {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int leng = Integer.parseInt(st.nextToken());
-        int[] numArr = new int[leng];
+        // 플레이어 수 정의(입력)
+        int playerCount = Integer.parseInt(st.nextToken());
 
-        System.out.println(numArr.length);
+        // 플레이어 이름들어가는 곳
+        String[] playerName = new String[playerCount];
 
-        numArr = new int[]{Integer.parseInt(st.nextToken())};
+        // 플레이어 이름 입력
+        for (int i = 0; i < playerName.length; i++) {
+            playerName[i] = br.readLine();
+        }
+        int max[] = new int[playerCount];
+        String[] test = new String[playerCount];
 
+        for (int i = 0; i < playerName.length; i++) {
+            if (test[i].charAt(0) != playerName[i].charAt(0)) {
+                test[i] = String.valueOf(playerName[i].charAt(0));
+            } else {
+                max[i] += 1;
+            }
+        }
 
-//        for (int i = 0; i <= numArr.length; i++) {
-//            numArr[i] = Integer.parseInt(st.nextToken());
-//        }
     }
 }
