1dc43966e30742d23f4f95df326e85848bd2ecad
manamjong <nskynet5374@naver.com>
2025-02-12 15:06:53 +0900

    long표현식

---
+++ src/java_pjt/chapter02/LongExample.java
@@ -0,0 +1,12 @@
+package java_pjt.chapter02;
+
+public class LongExample {
+    public static void main(String[] args) {
+        long var1 = 10;
+        // 범위를 벗어나 에러 최대 수 : 2,147,483,647
+        // long var2 = 22,147,483,647;
+        // long 타입은 대소문자로 표기해야 적용됨
+        // long 최댓값 => 9,223,372,036,854,775,807
+        long var2 = 220000000L;
+    }
+}
