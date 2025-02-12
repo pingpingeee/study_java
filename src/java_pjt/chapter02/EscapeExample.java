bdacb4f8ab998cf402946e1adc0844ee3a570adc
manamjong <nskynet5374@naver.com>
2025-02-12 15:05:58 +0900

    \t, \\ 등 문자리터럴

---
+++ src/java_pjt/chapter02/EscapeExample.java
@@ -0,0 +1,14 @@
+package java_pjt.chapter02;
+
+public class EscapeExample {
+    public static void main(String[] args) {
+        System.out.println("번호이름직업");
+//        백슬래시t => 탭만큼 띄움
+        System.out.print("번호\t이름\t직업");
+        System.out.println("번호\t이름\t직업");
+
+
+        System.out.println("\\개발자\\");
+        System.out.println("봄\\여름\\가을\\겨울");
+    }
+}
