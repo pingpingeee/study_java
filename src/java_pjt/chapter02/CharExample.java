37e4dcfd58ea1d2da44da05ab9d7449cc66edd47
manamjong <nskynet5374@naver.com>
2025-02-12 15:05:33 +0900

    유니코드, 16진수 표현

---
+++ src/java_pjt/chapter02/CharExample.java
@@ -0,0 +1,25 @@
+package java_pjt.chapter02;
+
+public class CharExample {
+    public static void main(String[] args) {
+        char c1 = 'A';
+//      유니코드 65 => A
+        char c2 = 65;
+//      \\u~ => 16진수
+        char c3 = '\u0041';
+
+        char c4 = '가';
+//        유니코드 44032 => 가
+        char c5 = 44032;
+//        ac00 => 16진수는 44032 10진수
+        char c6 = '\uac00';
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+        System.out.println(c4);
+        System.out.println(c5);
+        System.out.println(c6);
+    }
+}
