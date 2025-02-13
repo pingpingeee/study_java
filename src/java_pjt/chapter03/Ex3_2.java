8bf7ca375d72ba1c6998f54024073cb79b71ad6a
manamjong <nskynet5374@naver.com>
2025-02-13 17:25:11 +0900

    for문

---
+++ src/java_pjt/chapter03/Ex3_2.java
@@ -0,0 +1,9 @@
+package java_pjt.chapter03;
+
+public class Ex3_2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            System.out.println("3 x " + i + " = " + 3 * i);
+        }
+    }
+}
