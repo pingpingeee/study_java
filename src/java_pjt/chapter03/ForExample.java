cc0394d801f7a4f9b185076b36e8f0b75429941d
manamjong <nskynet5374@naver.com>
2025-02-13 17:25:54 +0900

    for

---
+++ src/java_pjt/chapter03/ForExample.java
@@ -0,0 +1,11 @@
+package java_pjt.chapter03;
+
+import java.sql.SQLOutput;
+
+public class ForExample {
+    public static void main(String[] args) {
+        for (int i = 0; i > 5; i++) {
+            System.out.println("i의 값 : " + i);
+        }
+    }
+}
