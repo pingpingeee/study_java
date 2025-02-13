4a9bd8e440fcf290e892ba072478a6e4d0495c2a
manamjong <nskynet5374@naver.com>
2025-02-13 17:25:04 +0900

    while문

---
+++ src/java_pjt/chapter03/Ex3_1.java
@@ -0,0 +1,12 @@
+package java_pjt.chapter03;
+
+public class Ex3_1 {
+    public static void main(String[] args) {
+        int n = 1;
+        while (n < 4) {
+            System.out.println(n);
+            n++;
+        }
+        System.out.println("END");
+    }
+}
