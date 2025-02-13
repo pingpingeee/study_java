556999d6ddd548a5fcb0e645b14e2d0b38aa3ae4
manamjong <nskynet5374@naver.com>
2025-02-13 17:20:02 +0900

    do while문

---
+++ src/java_pjt/chapter03/LoopExample3.java
@@ -0,0 +1,11 @@
+package java_pjt.chapter03;
+
+public class LoopExample3 {
+    public static void main(String[] args) {
+        int i = 10;
+        do {
+            System.out.println("i의 값 : " + i);
+            i--;
+        } while (i < 3);
+    }
+}
