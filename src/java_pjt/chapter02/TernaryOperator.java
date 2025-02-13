19ae5d5a1b2a5c68dc1f9cd802f4642dc19e03da
manamjong <nskynet5374@naver.com>
2025-02-13 11:00:14 +0900

    삼항연산

---
+++ src/java_pjt/chapter02/TernaryOperator.java
@@ -0,0 +1,9 @@
+package java_pjt.chapter02;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int a = 3, b = 5;
+//        a > b가 거짓이므로 b -a를 반환
+        System.out.println("두 수의 차 : " + ((a > b) ? (a - b) : (b - a)));
+    }
+}
