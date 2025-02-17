bdad95da82b54d061046ef11740dfdc7ee88b277
manamjong <nskynet5374@naver.com>
2025-02-17 10:18:42 +0900

    2중 for문 역별찍기 실습

---
+++ src/java_pjt/question04/PrintAsterisk.java
@@ -0,0 +1,18 @@
+package java_pjt.question04;
+
+import java.util.Scanner;
+
+public class PrintAsterisk {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("정수 입력 : ");
+        int num = sc.nextInt();
+
+        for (int i = num; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
