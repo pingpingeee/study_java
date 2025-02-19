8a800203e78308c99bb68c0e8f49ee017cc0fb83
manamjong <nskynet5374@naver.com>
2025-02-19 14:41:50 +0900

    접근지정자 기초

---
+++ src/Access/package1/A.java
@@ -0,0 +1,6 @@
+package Access.package1;
+
+//public class A { // public 접근지정자(접근 제한 없음)
+class A { // default 접근지정자(같은 패키지 접근 가능)
+//private class A { // 오류
+}
---
+++ src/Access/package2/C.java
@@ -0,0 +1,11 @@
+package Access.package2;
+
+//import Access.package1.A;
+
+import Access.package1.B; // 다른 패키지의 클래스는 import 사용
+
+public class C {
+    B b;
+//    default 접근지정자는 다른 패키지에서 접근시 오류
+//    A a;
+}
