ec9c376ebbe2d7be02af90fb5a4e81625a7bfedf
manamjong <nskynet5374@naver.com>
2025-02-19 12:26:29 +0900

    split직접만들기

---
+++ src/java_pjt/chapter04/ArrayParameterEx.java
@@ -0,0 +1,26 @@
+package java_pjt.chapter04;
+
+public class ArrayParameterEx {
+
+    // 공백을 쉼표로 바꾸는 메소드
+    static void replaceSpace(char a[]) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == ' ') a[i] = ',';
+        }
+    }
+
+    static void printCharArray(char a[]) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        char c[] = {'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.'};
+
+        printCharArray(c);
+        replaceSpace(c);
+        printCharArray(c);
+    }
+}
