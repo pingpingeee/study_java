dc479ae43e611d6e583fa4de899c3d3d0b424d33
manamjong <nskynet5374@naver.com>
2025-02-21 11:24:34 +0900

    인터페이스 메인

--- src/java_pjt/chapter05_01_interface/Main.java
+++ src/java_pjt/chapter05_01_interface/Main.java
@@ -3,23 +3,38 @@ package java_pjt.chapter05_01_interface;
 public class Main {
     public static void main(String[] args) {
 //        new Animal(); // 추상클래스는 객체 생성 X
-        Animal a; // 참조변수 선언은 가능
+        Animal animal; // 참조변수 선언은 가능
 //        new Bird(); // 추상클래스는 객체 생성 X
-        Penguin p = new Penguin();
-        p.swim(); // 멤버 5개 (life, wing, walk(), eat(), swim()
+        Penguin penguin1 = new Penguin();
+        penguin1.swim(); // 멤버 5개 (life, wing, walk(), eat(), swim()
 
-        Bird p2 = new Penguin(); // 업캐스팅
+        Bird penguin2 = new Penguin(); // 업캐스팅
 //        p2.swim(); // 업캐스팅은 슈퍼클래스의 멤버만 접근 가능(오류 발생)
-        p2.walk();// 업캐스팅은 슈퍼클래스의 멤버만 접근 가능
-        Penguin p3 = (Penguin) p2; // 다운캐스팅
-        p3.swim();// 다운캐스팅은 모든 멤버 접근 가능
+        penguin2.walk();// 업캐스팅은 슈퍼클래스의 멤버만 접근 가능
+        Penguin penguin3 = (Penguin) penguin2; // 다운캐스팅
+        penguin3.swim();// 다운캐스팅은 모든 멤버 접근 가능
 
-        Bird d = new Duck(); // 업캐스팅
-        d.eat(); // 업캐스팅은 슈퍼클래스의 멤버만 접근 가능
-        Duck d2 = (Duck) d; // 다운캐스팅
-        d2.fly(); // 다운캐스팅은 모든 멤버 접근 가능
+        Bird duck1 = new Duck(); // 업캐스팅
+        duck1.eat(); // 업캐스팅은 슈퍼클래스의 멤버만 접근 가능
+        Duck duck2 = (Duck) duck1; // 다운캐스팅
+        duck2.fly(); // 다운캐스팅은 모든 멤버 접근 가능
 
 //        추상클래스를 참조변수 선언(슈퍼클래스)
-        Animal d3 = new Duck(); // 업캐스팅은 슈퍼클래스의 멤버만 접근가능
+        Animal duck3 = new Duck(); // 업캐스팅은 슈퍼클래스의 멤버만 접근가능
+        int x = duck3.life;
+        System.out.println("x => " + x);
+
+//        new Swim2(); // 인터페이스는 객체 생성 X
+        Swim2 swim = new Penguin(); // 인터페이스를 참조변수 선언
+        swim.swim();
+
+        // 인터페이스가 인터페이스 배열 2개를 가리킨다.
+        // 객체배열과 형태가 비슷
+        Swim2[] swim2 = new Swim2[2];
+        swim2[0] = new Penguin();
+        swim2[1] = new Duck();
+        swim2[0].swim();
+        swim2[1].swim();
+
     }
 }
