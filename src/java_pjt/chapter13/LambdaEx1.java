a3738a563ddb70d35cdaa79cd205c53df61fa1bc
manamjong <nskynet5374@naver.com>
2025-02-25 17:28:06 +0900

    람다식1

---
+++ src/java_pjt/chapter13/LambdaEx1.java
@@ -0,0 +1,18 @@
+package java_pjt.chapter13;
+
+// 람다식으로 구현할 함수형 인터페이스
+interface MyFunction {
+    int calc(int x, int y); // 람다식으로 구현할 추상메소드
+}
+
+public class LambdaEx1 {
+    public static void main(String[] args) {
+        MyFunction add = (x, y) -> {
+            return x + y;
+        }; // 람다식
+        MyFunction sub = (x, y) -> x - y; // 람다식 {}와 return 생략
+
+        System.out.println(add.calc(1, 2)); // 합 구하기(람다식 객체에서 calc 메소드 호출)
+        System.out.println(sub.calc(1, 2)); // 차 구하기(람다식 객체에서 calc 메소드 호출)
+    }
+}
